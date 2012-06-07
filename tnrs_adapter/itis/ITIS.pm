f40f2370791098eede901cafe7a99874d1b8e8c0
gaurav <gaurav@ggvaidya.com>
2012-06-07 12:22:40 -0400

    ITIS adaptor can now look up the accepted name for the name that it found.

--- tnrs_adapter/itis/ITIS.pm
+++ tnrs_adapter/itis/ITIS.pm
@@ -34,6 +34,8 @@ BEGIN {
 use Carp;
 use LWP::UserAgent;
 use JSON;
+use XML::Entities;
+
 use MyInterfaces::ITISService::ITISServiceHttpSoap11Endpoint;
 
 =head2 new
@@ -64,68 +66,106 @@ ITIS TNRS for each of the names.
 
 sub lookup {
     my ($self, @names) = @_;
+    my @returned_names;
 
     croak "No names provided!" if (0 == scalar @names);
 
-    # Make the API call.
-    my $names = join(',', @names); 
+    foreach my $name (@names) {
 
-    my $itis = $self->{'itis'};
-    if (not defined $itis) {
-        $itis = $self->{'itis'} = MyInterfaces::ITISService::ITISServiceHttpSoap11Endpoint->new();
-    }
+        # Make the API call.
+        my $itis = $self->{'itis'};
+        if (not defined $itis) {
+            $itis = $self->{'itis'} = MyInterfaces::ITISService::ITISServiceHttpSoap11Endpoint->new();
+        }
 
-    my $response = $itis->getITISTermsFromScientificName({
-        srchKey => 'Mangifera indica'
-    });
+        my $response = $itis->getITISTermsFromScientificName({
+            srchKey => $name
+        });
 
-    use Data::Dumper;
+        # All terms.
+        my @terms = @{$response->get_return()->get_itisTerms()};
 
-    # This is a scientificName.
-    die $response->get_return()->get_itisTerms()->get_tsn();
-# ->{'scientificNames'}->{'combinedName'};
+        # ITIS does a simple text search against its entire database.
+        # So let's try to find an ITIS term with a scientific name
+        # that precisely matches what we currently have.
+        my $term_to_use;
+        foreach my $term (@terms) {
+            my $scientificName = $term->get_scientificName();
+            next unless defined $scientificName;
 
+            my $decoded_name = $scientificName;
+            $decoded_name = XML::Entities::decode('all', $scientificName)
+                if ($scientificName ne '');
+            
+            if($decoded_name eq $name) {
+                # A match! Use this term. 
+                $term_to_use = $term;
+                last;
+            }
+        }
 
-    # If no success, report an error.
-    unless($response->is_success) {
-        return {
-            'status' => $response->code,
-            'errorMessage' => $response->message
-        };
-    }
+        unless(defined $term_to_use) {
+            push @returned_names, {
+                'submittedName' => $name,
+                'matchedName' => "",
+                'acceptedName' => "",
+                'uri' => "",
+                'annotations' => {},
+                'score' => 0
+            };
+            next;
+        }
 
-    # Pull out the data we need and construct a JSON object to return.
-    my $results = decode_json($response->decoded_content(
-        charset => 'none'   
-            # Without this, I think decode_json tries to
-            # re-decode Unicode characters; simpler to let
-            # decode_json do the decoding, I think.
-    ));
+        # Figure out the accepted name.
+        my $tsn = $term_to_use->get_tsn();
+        $response = $itis->getAcceptedNamesFromTSN({
+            tsn => $tsn
+        }); 
+        my $warning;
+        my $accepted_name;
+        my $accepted_tsn;
+        my @accepted_names = @{$response->get_return()->get_acceptedNames()};
 
-    my @returned_names;
-    foreach my $item (@{$results->{'items'}}) {
-        my $name = {
-            'submittedName' => $item->{'nameSubmitted'},
-            'matchedName' => $item->{'nameScientific'},
-            'acceptedName' => $item->{'acceptedName'},
-            'uri' => $item->{'acceptedNameUrl'},
+        if(0 == scalar @accepted_names) {
+            $accepted_name = "";
+            $accepted_tsn = "";   
+
+            warn "No accepted name found for $name/$tsn on ITIS.";
+
+        } elsif(1 != scalar @accepted_names) {
+            # I don't know what it means for ITIS to have multiple
+            # accepted names for a single TSN; so right now, I'll
+            # bail out if we hit that case.
+            die("Several accepted names found on ITIS for $name/$tsn:\n" . join("\n\t", @accepted_names));
+
+        } else {
+            if($accepted_names[0] eq "") {
+                # This appears to be ITIS behavior for when the provided
+                # TSN is the accepted name.
+                $accepted_name = $name;
+                $accepted_tsn = $tsn;
+            } else {
+                $accepted_name = $accepted_names[0]->get_acceptedName();
+                $accepted_tsn = $accepted_names[0]->get_acceptedTsn();
+            }
+        }
+
+        push @returned_names, {
+            'submittedName' => $name,
+            'matchedName' => ($term_to_use->get_scientificName()) . "", # Cast it to a string
+            'acceptedName' => $accepted_name . "",
+            'uri' => "http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=$accepted_tsn",
             'annotations' => {
-                'Authority' => $item->{'acceptedAuthor'}
+                'originalTSN' => $tsn . "",
+                'TSN' => $accepted_tsn . ""
             },
-            'score' => $item->{'overall'}
+            'score' => 0.5
         };
-
-        push @returned_names, $name;
-    }
-
-    my $error_message = $response->message;
-    if($error_message eq 'OK') {
-        $error_message = "";
     }
 
     return {
-        'status' => $response->code,
-        'errorMessage' => $error_message,
+        'status' => 200,
+        'errorMessage' => "",
         'names' => \@returned_names
     };
 }
