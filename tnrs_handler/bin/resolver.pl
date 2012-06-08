37f0345f4b83fc079b756955ed5badd320fe9f89
Naim Matasci <nmatasci@iplantcollaborative.org>
2012-06-07 19:26:13 -0700

    Functioning prototype

--- tnrs_handler/bin/resolver.pl
+++ tnrs_handler/bin/resolver.pl
@@ -1,67 +1,52 @@
-
+#!/usr/bin/env perl
 use strict;
 use JSON;
 
-our @VERSION=1.0;
+our @VERSION = 1.0;
 
-test();
-sub test{
-my$ad_ref=load_adapters();
-open(my$TD,"</home/nmatasci/TNRastic/testdata/test.json") or die "Cannot open testdata: $!\n";
-my@test=(<$TD>);
-close $TD;
+process(@ARGV);
+exit 0;
 
-my$res = decode_json( join '',@test );
-my@results;
-		$res->{sourceId}=0;
-		$res->{sourceRank}=0;
-		push @results, $res;
-		my%res2=%{$res};
-		$res2{sourceId}=1;
-		$res2{sourceRank}=1;
-		push @results, \%res2;
-		$res=merge(\@results);
-		write_output($res,"/home/nmatasci/TNRastic/testdata/newtest.out",'newtest',localtime,'');
-}
+sub process {
+	my $names_file    = shift;
+	my $adapters_file = shift;
+	my $target_dir    = shift;
 
-
-sub process{
-	my$names_file=shift;
-	my$adapters_file=shift;
-	my$target_dir=shift;
-	
 	#output name
-	my$jobId=(split qr(\/), $names_file)[-1];
-	$jobId=(split(qr(\.),$jobId))[0];
-	
+	my $jobId = ( split qr(\/), $names_file )[-1];
+	$jobId = ( split( qr(\.), $jobId ) )[0];
+
 	#date
-	my$sub_date=localtime;
-	
-	#load the names to process
-	open(my$NF,"<$names_file") or return "Cannot load names file: $!\n";
-	my@names=(<$NF>);
-	close $NF;
-	
+	my $sub_date = localtime;
+
 	#load adapters registry
-	my$ad_ref=load_adapters($adapters_file);
-	my$res=query_sources(join('',@names),$ad_ref);
-	$res=merge($res);
-	write_output($res,"$target_dir/$jobId.json",$jobId,$sub_date, $ad_ref);
-	kill $names_file;
+	my $ad_ref = load_adapters($adapters_file);
+	my $res = query_sources( $names_file, $ad_ref );
+	$res = merge($res);
+	write_output( $res, "$target_dir/$jobId.json", $jobId, $sub_date, $ad_ref );
+	unlink $names_file;
 	return 0;
 }
 
 #TODO: Parallelize
 #send the name batch to each adapter
-sub query_sources{
-	my$names_file=shift;
-	my$adapters_ref=shift;
-	my@results;
-	foreach(@{$adapters_ref}){
-		my%source=%{$_};
-		my$res=decode_json(system("cat $names_file | $source{call}")); 
-		$res->{sourceId}=$source{id};
-		$res->{sourceRank}=$source{rank};
+sub query_sources {
+	my $names_file   = shift;
+	my $adapters_ref = shift;
+	my @results;
+	foreach ( @{ $adapters_ref->{adapters} } ) {
+		my %source = %{$_};
+
+		my $input =
+		  `cat $names_file | $source{call} 2>/dev/null`
+		  ;    #TODO: redirect STDERR to logs
+		my $res;
+		eval {
+			$res = decode_json($input);
+
+			$res->{sourceId}   = $source{id};
+			$res->{sourceRank} = $source{rank};
+		};
 		push @results, $res;
 	}
 	return \@results;
@@ -69,91 +54,92 @@ sub query_sources{
 
 #merge names returned by adapters
 sub merge {
-	my$results=shift;
-	my%matches;
-	my%output;
-	foreach(@{$results}){ #for every source
-		my$res=$_;
-		if($res->{status} ne 200){ #there was a failure
-#TODO: Error message should go in the job source metadata
-			next;	
+	my $results = shift;
+	my %matches;
+	my %output;
+	foreach ( @{$results} ) {    #for every source
+		my $res = $_;
+		if ( $res->{status} ne 200 ) {    #there was a failure
+
+			#TODO: Error message should go in the job source metadata
+			next;
 		}
-		my$sourceid=$res->{sourceId};
-		my$rank=$res->{sourceRank};
-		my@names=@{$res->{names}}; 
-		foreach(@names){ #for all the submitted names
-			my%input=%{$_};
-			if(!$input{matchedName} || $input{matchedName} eq 'null'){
-				next;	
+		my $sourceid = $res->{sourceId};
+		my $rank     = $res->{sourceRank};
+		my @names    = @{ $res->{names} };
+		foreach (@names) {                #for all the submitted names
+			my %input = %{$_};
+			if ( !$input{matchedName} || $input{matchedName} eq 'null' ) {
+				next;
 			}
 
-
 			#builds the output object
-			my$output = {
-				sourceId=>$sourceid,
-				matchedName=>$input{matchedName},
-				acceptedName=>$input{acceptedName},
-				uri=>$input{uri},
-				score=>$input{score},
-				annotations=>$input{annotations},
-				
+			my $output = {
+				sourceId     => $sourceid,
+				matchedName  => $input{matchedName},
+				acceptedName => $input{acceptedName},
+				uri          => $input{uri},
+				score        => $input{score},
+				annotations  => $input{annotations},
+
 			};
-			@{$matches{$input{submittedName}}}[$rank]=$output;
+			@{ $matches{ $input{submittedName} } }[$rank] = $output;
 
-		} #end of matches
+		}    #end of matches
 
-	} #end of source
+	}    #end of source
 	return \%matches;
 }
 
+sub write_output {
+	my $results  = shift;
+	my $filename = shift;
+	my $jobid    = shift;
+	my $sub_date = shift;
+	my $sources  =
+	  extract_meta(shift);    #extract the source metadata from the adapters
 
-sub write_output{
-	my$results=shift;
-	my$filename=shift;
-	my$jobid=shift;
-	my$sub_date=shift;
-	my$sources=extract_meta(shift); #extract the source metadata from the adapters
-	
-	my$output;
-	
-	
-	my$meta={
-		jobId => $jobid,
-		sources => $sources,
+	my $output;
+
+	my $meta = {
+		jobId    => $jobid,
+		sources  => $sources,
 		sub_date => $sub_date
 	};
-	$output->{metadata}=$meta;
-	my@names;
-	
-	for my$key (keys %{$results}){
-		my@matches=@{$results->{$key}};
-		my$entry ={
-			submittedName=> $key,
-            matchCount=> scalar(@matches),
-            matches => \@matches		
+	$output->{metadata} = $meta;
+	my @names;
+
+	for my $key ( keys %{$results} ) {
+		my @matches = @{ $results->{$key} };
+		my $entry   = {
+			submittedName => $key,
+			matchCount    => scalar(@matches),
+			matches       => \@matches
 		};
-		push @names, $entry;	
+		push @names, $entry;
 	}
-	
-	$output->{names}=\@names;
-	
+
+	$output->{names} = \@names;
+
 	$output = encode_json($output);
-	open(my$OF,">$filename") or return "Cannot open output file $filename: $!\n";
+	open( my $OF, ">$filename" )
+	  or return "Cannot open output file $filename: $!\n";
 	print $OF $output;
 	close $OF;
 	return 0;
 }
 
-#Extract metadata from the sources
-sub extract_meta{
-	return my%dummy;
+#TODO: Extract metadata from the sources
+sub extract_meta {
+	return my %dummy;
 }
 
-sub load_adapters{
-	my$adapters_file=shift;
-	open(my$ADA, "<$adapters_file") or die "Cannot load adapter configuration file $adapters_file: $!";
-	my@adapters=(<$ADA>);
+sub load_adapters {
+	my $adapters_file = shift;
+	open( my $ADA, "<$adapters_file" )
+	  or die "Cannot load adapter configuration file $adapters_file: $!";
+	my @adapters = (<$ADA>);
 	close $ADA;
-	my$adapters_ref=decode_json(join '',@adapters); 
+	my $adapters_ref = decode_json( join '', @adapters );
 	return $adapters_ref;
 }
--- tnrs_handler/lib/tnrs_handler.pm
+++ tnrs_handler/lib/tnrs_handler.pm
@@ -1,77 +1,111 @@
 package tnrs_handler;
 use Dancer ':syntax';
+use Parallel::ForkManager;
 use JSON;
 use Digest::MD5 qw(md5_hex);
 
-#use tnrs_handler::resolver;
-
 our $VERSION = '0.1';
+
 #my$config_file_path="/home/nmatasci/test.cfg";
 ##my$cfg=load_config($config_file_path);
 #my$adapters_file=$cfg->{adapters};
 #my$tempdir=$cfg->{tempdir};
 #my$storage=$cfg->{storage};
-my$storage="/home/nmatasci/TNRastic/testdata";
+my $storage       = "/home/nmatasci/TNRastic/testdata/out";
+my $adapters_file = "tnrs_adapter/adapters.json";
+my $host          = "http://128.196.142.37:3000";
+my $tempdir       = "/tmp";
+my $n_pids        = 0;
+my $MAX_PIDS      = 5;
 
-#prefix '/1';
-
+#TODO: Date format
+#TODO: Config loader
+#TODO: Count sources
+#TODO: Sources metadata
 
 get '/' => sub {
-    template 'index';
+	template 'index';
+};
+any [ 'get', 'post' ] => '/status' => sub {
+	return "Up!\n";
 };
 
-any ['post'|'get'] => '/submit'  => sub {
-	#examine contents
-	my$para=request->params;
+any [ 'post', 'get' ] => '/submit' => sub {
 
-	if(!defined($para)){
+	#examine contents
+	wait;
+	my $para = request->params;
+
+	$para->{query};
+	if ( !defined($para) ) {
 		status 'bad_request';
-		return "Please specify either an input file or a list of names\n";	
+		return "Please specify a list of newline separated names\n";
 	}
-	else{
-		return "1234";	
+	else {
+		my $names = $para->{query};
+		my $fn = md5_hex( $names, time );
+		open( my $TF, ">$tempdir/$fn.tmp" ) or error_code('generic');
+		print $TF $names;
+		close $TF;
+
+		my $status = submit("$tempdir/$fn.tmp");
+
+		my $uri  = "$host/$fn";
+		my $date = localtime;
+		my $json = {
+			"submit date" => $date,
+			token         => $fn,
+			uri           => "$host/retrieve/$fn",
+			message       =>
+"Your request is being processed. You can retrieve the results at $host/retrieve/$fn."
+		};
+		return encode_json($json);
 	}
 };
 
 get '/retrieve/:job_id' => sub {
-	my$job_id=param('job_id');
-	if(-f "$storage/$job_id.out"){
-		open (my$RF,"<$storage/$job_id.out") or error_code("generic");
-		my@tmp=(<$RF>);
+	wait;
+	my $job_id = param('job_id');
+	if ( -f "$storage/$job_id.json" ) {
+		open( my $RF, "<$storage/$job_id.json" ) or error_code("generic");
+		my @tmp = (<$RF>);
 		close($RF);
-		return join '',@tmp;
-	} 
-#TODO: add test for freshness: if request is older then 24h, then return 404
-	status 'accepted';
-	return "Job $job_id is still being processed\n";
-	
-	
+		return join '', @tmp;
+	}
+	elsif ( -f "$tempdir/$job_id.tmp" ) {
+
+	#TODO: add test for freshness: if request is older then 24h, then return 404
+		status 'accepted';
+		return "Job $job_id is still being processed.\n";
+	}
+	else {
+		status 'not_found';
+		return "Error. Job $job_id doesn't exits.\n";
+	}
+
 };
 
-sub error_code{
+sub error_code {
 	status 'internal_server_error';
 	return "General error. Please retry later\n";
-	
+
 }
 
+sub submit {
+	wait;
+	my $filename = shift;
+	my $pm       = new Parallel::ForkManager(2);
 
-
-sub submit{
-	my$names=shift;
-	my$filename=shift;
-	#create temp file
-	open(my $TF, ">$filename.tmp");
-	#call resolver.pl w name file	
-	my $pid = fork;
-	if(!defined($pid)) {
-		return "Unable to fork $!";
+	$n_pids++;
+	my $pid = $pm->start and return;
+	if ( $n_pids >= $MAX_PIDS ) {
+		sleep $n_pids * 10;
 	}
-    exec "./resolver.pl $names";  # Some long running process.
-		return "Unable to start resolver: $!"
+	system "./resolver.pl $filename $adapters_file $storage"
+	  ;    # Some long running process.
+	$n_pids--;
+	$pm->finish;
+
 }
-	
-	
-
-
 
 true;
